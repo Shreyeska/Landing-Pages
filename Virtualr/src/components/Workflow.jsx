3c8320168b76468aa05c1cf8b90af6554dce8b43
Shreyeska <silwalshreyeska@gmail.com>
2025-07-21 21:19:54 +0545

    Update: Basic WorkFlow Setup

---
+++ Virtualr/src/components/Workflow.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { CheckCircle2 } from "lucide-react";
+import codeImg from "../assets/code.jpg";
+
+const Workflow = () => {
+  return (
+    <div className="mt-20">
+      <h2 className=" text-3xl sm:text-5xl lg:text-6xl  text-center mt-6 tracking-wide">
+        Accelerate your coding workflow.
+      </h2>
+    </div>
+  );
+};
+
+export default Workflow;
